65124674badf076f1300ff093f92d03a33e35fa8
Meet Gandhi <meetgandhi14@gmail.com>
2023-02-04 11:13:22 -0500

    added model and architecture

--- assignments/week3/model.py
+++ assignments/week3/model.py
@@ -2,7 +2,7 @@ import torch
 from typing import Callable
 
 
-class MLP:
+class MLP(torch.nn.Module):
     def __init__(
         self,
         input_size: int,
@@ -14,15 +14,47 @@ class MLP:
     ) -> None:
         """
         Initialize the MLP.
-
-        Arguments:
-            input_size: The dimension D of the input data.
-            hidden_size: The number of neurons H in the hidden layer.
-            num_classes: The number of classes C.
-            activation: The activation function to use in the hidden layer.
-            initializer: The initializer to use for the weights.
+        Args:
+            input_size (int): The dimension D of the input data.
+            hidden_size (int): The number of neurons H in the hidden layer.
+            num_classes (int): The number of classes C.
+            hidden_count (int, optional): The number of hidden layers. Defaults to 1.
+            activation (Callable, optional): The activation function to use in the
+            hidden layer. Defaults to torch.nn.ReLU.
+            initializer (Callable, optional): The initializer to use for the weights.
+            Defaults to initializer.
         """
-        ...
+        super().__init__()
+
+        self.actv = activation()
+
+        if hidden_count >= 2:
+            self.layers = torch.nn.ModuleList()
+            for i in range(hidden_count):
+                if i == 0:
+                    layer = torch.nn.Linear(input_size, hidden_size)
+                elif i == hidden_count - 1:
+                    layer = torch.nn.Linear(hidden_size, num_classes)
+                else:
+                    layer = torch.nn.Linear(hidden_size, hidden_size)
+
+                initializer(layer.weight)
+                self.layers += [layer]
+
+        elif hidden_count == 1:
+            input_layer = torch.nn.Linear(input_size, hidden_size)
+            initializer(input_layer.weight)
+
+            output_layer = torch.nn.Linear(hidden_size, num_classes)
+            initializer(output_layer.weight)
+
+            self.layers = torch.nn.ModuleList([input_layer, output_layer])
+
+        elif hidden_count == 0:
+            layer = torch.nn.Linear(input_size, num_classes)
+            initializer(layer.weight)
+
+            self.layers = torch.nn.ModuleList([layer])
 
     def forward(self, x):
         """
@@ -34,4 +66,7 @@ class MLP:
         Returns:
             The output of the network.
         """
-        ...
+        for layer in self.layers:
+            x = layer(x)
+            x = self.actv(x)
+        return x
